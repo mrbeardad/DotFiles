a9eaad524b9c4a457a17c4549a0ffc4a517a89e7
Heachen Bear <960720535@qq.com>
2020-03-11 20:24:33 +0800

    today, I replaced GDB with LLDB
    GDB always crashs when type some command. Although debug a 'hello world'

--- init.sh
+++ init.sh
@@ -6,15 +6,17 @@ function ssh_cfg() {
     cp -f ssh/ssh_config ~/.ssh/ssh_config
     sudo systemctl enable --now sshd.socket
     sudo cp ssh/motd /etc/motd
-    echo -e "\e[33m Note:\e[m Now you need to push your ~/.ssh/id_ecdsa.pub to your github account"
-    echo -e 'To generate public private key, run command: \e[32mssh-keygen -t ecdsa -b 521 -C "yourname@youremail"'
+    echo -e "\e[32m=====> SSH\e[m
+    Now you need to push your ~/.ssh/id_ecdsa.pub to your github account."
+
 }
 
 
 #配置zsh
 function zsh_cfg() {
     #下载oh-my-zsh
-    yay -S oh-my-zsh-git powerline-fonts zsh-syntax-highlighting zsh-autosuggestions autojump
+    yay -S oh-my-zsh-git
+    yay -S zsh powerline-fonts zsh-syntax-highlighting zsh-autosuggestions autojump
 
     #添加alias和man()
     cp -f zsh/.zshrc ~/.zshrc
@@ -70,11 +72,11 @@ function vim_cfg() {
     cp  vim/gvimrc ~/.vim/gvimrc
     g++ -O3 -o ~/.local/bin vim/time.cpp
 
-    yay -S gvim vim-plug cmake ctags gperf vim-instant-markdown vim-youcompleteme-git cppcheck
+    yay -S gvim vim-plug cmake ctags gperf vim-instant-markdown cppcheck archlinuxcn/nerd-fonts-complete
 
-    echo -e 'The plugin "LeaderF" can work with gtags, you can download it at http://tamacom.com/global/global-6.6.4.tar.gz ,and you need to compile it in your machine.'
-    echo -e ' How to do is written on the website, and donot forget to \e33m"sudo make install"'
-    echo -e '\e[32mStartup your vim and run the command ":PlugInstall"'
+    echo -e '\e[32m=====> VIM\e[m
+    Now, launch your vim and run the command ":PlugInstall"
+    The vim plugin "LeaderF" can work with gtags, you can download it at http://tamacom.com/global/global-6.6.4.tar.gz ,and you need to compile it on your machine.'
 }
 
 
@@ -85,7 +87,7 @@ function extra_cfg() {
     cd /opt
     sudo unzip ~/Downloads/chfs-linux-amd64-1.8.zip
     chmod 755 chfs-linux-amd64-1.8/chfs
-    ln -s /opt/chfs-linux-amd64-1.8/chfs bin/chfs
+    sudo ln -s /opt/chfs-linux-amd64-1.8/chfs bin/chfs
     cd -
     sudo cp chfs/chfs.{service,socket} /etc/systemd/system/
     sudo mkdir /srv/chfs
@@ -96,68 +98,85 @@ function extra_cfg() {
     #archibold
     sudo curl -o /opt/bin http://archibold.io/sh/archibold
     sudo chmod 755 /opt/bin
+    echo -e "\e[32m=====> GDB
+    Now, you can change your gdm background by run 'archibold login-backgroud /path/to/your/picture'"
 
     #CLI工具
-    yay -S sendemail htop iotop ncdu tldr cloc screenfetch ranger figlet cmatrix cheat dstat ntfs-3g archlinuxcn/cppman-git gdb
+    yay -S sendemail htop iotop ncdu tldr cloc screenfetch ranger figlet cmatrix cheat dstat ntfs-3g archlinuxcn/cppman-git clang lldb
 
     #百度网盘，QQ，网易云音乐，搜狗拼音，WPS
-    yay -S baidunetdisk-bin deepin.com.qq.im wqy-microhei netease-cloud-music vlc fcitx-sogoupinyin fcitx-im fcitx-configtool wps-office ttf-wps-fonts flameshot google-chrome
+    yay -S baidunetdisk-bin deepin.com.qq.im deepin.com.qq.office wqy-microhei netease-cloud-music vlc fcitx-sogoupinyin fcitx-im fcitx-configtool wps-office ttf-wps-fonts flameshot google-chrome
+    git clone https://github.com/haotian-wang/google-access-helper ~/Downloads/google-access-helper
+
+    echo -e '\e[32=====> Chrome
+    Now, add google-access-helper to your google-chrome in devloper mode'
+
     #搜狗拼音配置
     echo -e 'export GTK_IM_MODULE=fcitx\nexport GTK_IM_MODULE=fcitx\nexport XMODIFIERS="@im=fcitx"' > ~/.xprofile
-    #WPS配置
-    sed -i '1a\export XMODIFIERS="@im=fcitx"\nexport QT_IM_MODULE="fcitx"\n' /usr/bin/wps
+
+    #WPS配置，现在好像不需要了。。
+    #sudo sed -i '1a\export XMODIFIERS="@im=fcitx"\nexport QT_IM_MODULE="fcitx"\n' /usr/bin/wps
+
+    #QQ配置
+    sudo echo "net.ipv6.conf.all.disable_ipv6 =1
+    net.ipv6.conf.default.disable_ipv6 =1
+    net.ipv6.conf.lo.disable_ipv6 =1" >> /etc/sysctl.conf
+    sudo sysctl -p /etc/sysctl.conf
 
     #GNOME扩展
     yay -S gtk-theme-macos-mojave adapta-gtk-theme-bin breeze-hacked-cursor-theme breeze-adapta-cursor-theme-git ttf-google-fonts-git tela-icon-theme-git gnome-shell-extension-coverflow-alt-tab gnome-shell-extension-system-monitor-git
 }
 
 
-# function main() {
+# function main() { 安装完后就改个sudo配置，其他啥也不用动
 
-# Basic configuration
+# 系统配置
 timedatectl set-ntp 1 && timedatectl set-local-rtc 1
-sudo systemctl disable --now bluetooth.target
 sudo systemctl disable --now bluetooth.service
 sudo systemctl disable --now  org.cups.cupsd.service
 sudo systemctl disable --now  org.cups.cupsd.socket
 sudo systemctl enable --now fstrim.timer
 sudo sed -i '/\[Journal\]/a\SystemMaxUse=50M' /etc/systemd/journald.conf
-sudo bash -c "mv /usr/local/* /opt && rmdir /usr/local && ln -s /usr/local /opt"
-echo -e "\e[33mNow you may need to edit your /etc/fstab to add a swap filesystem and change both <dump> and <pass> filed to 0 for all filesystem\e[m"
+sudo bash -c "mv /usr/local/* /opt && rmdir /usr/local && ln -s /opt /usr/local"
+echo -e "\e[32m=====> /etc/fstab\e[m
+Now you may need to edit your /etc/fstab to add a swap filesystem
+and change both <dump> and <pass> filed to 0 for all filesystem"
 
-# pacman configuration
+# pacman配置
 sudo bash -c "echo 'Server = https://mirrors.cloud.tencent.com/manjaro/stable/\$repo/\$arch
 Server = https://mirrors.ustc.edu.cn/manjaro/stable/\$repo/\$arch
 Server = https://mirrors.tuna.tsinghua.edu.cn/manjaro/stable/\$repo/\$arch' > /etc/pacman.d/mirrorlist"
-sudo cp /etc/pacman.d/mirrorlist{,.bak} # Sometimes your mirrorlist will be changed after update your system
+sudo cp /etc/pacman.d/mirrorlist{,.bak} # 有时候更新系统会把这个也更新了，备份一下
 sudo sed -i "/#Color/s/#//" /etc/pacman.conf
 sudo bash -c "echo '[archlinuxcn]
 SigLevel = Optional TrustAll
-Server = https://mirrors.cloud.tencent.com/archlinuxcn/\$arch' >> /etc/pacman.conf"
+Server = https://mirrors.163.com/archlinux-cn/\$arch' >> /etc/pacman.conf"
 sudo pacman -S yay
 yay -S aria2 expac pacman-contrib-git base-devel
 sudo systemctl enable --now paccache.timer
 
-# grub
+# grub配置
 sudo cp grub/01_users /etc/grub.d
-echo "Please type a password 2 times for grub..."
+echo -e "Please type a password 2 times for grub...\n=> "
 sudo bash -c "grub-mkpasswd-pbkdf2 > /boot/grub/user.cfg"
 sudo sed -i '1,2d; 3s/.*is /GRUB_PASSWORD=/;' /boot/grub/user.cfg
 sudo sed -i '/--class os /s/--class os /--class os --unrestricted /' /etc/grub.d/{10_linux,30_os-prober}
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-echo -e "\e[33mNow you may need to change your grub theme"
-echo "\e[33mNow you may need to change your X-server to wayland"
+echo -e "\e[32m=====> GRUB\e[m
+Now you may need to change your grub theme.Some version of Manjaro don't install grub-theme-manjaro.
+And you can may like to change the default .png picture. https://www.yasuotu.com/mgeshi provide online picture converter"
 
-# Nvidia driver
+# Nvidia配置
 yay -S bumblebee virtualgl lib32-virtualgl lib32-primus primu
 sudo systemctl enable --now bumblebee
 sudo gpasswd -A beardad bumblebee
+echo -e '\e[32m=====> patches
+Now, you may want to apply patches in patches directory'
 
 ssh_cfg
 zsh_cfg
 vim_cfg
 tmux_cfg
 extra_cfg
-echo "Now you may need to apply these patches."
 # }
 
