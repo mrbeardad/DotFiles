e47e70c086bdf927fb61f756cdef54088a00705d
Heachen Bear <960720535@qq.com>
2020-04-11 14:37:08 +0800

    update init.sh

--- init-for-myself.sh
+++ init-for-myself.sh
@@ -10,13 +10,14 @@ function ssh_cfg() {
     sudo cp -v ssh/sshd_config /etc/ssh/sshd_config
     sudo cp -v ssh/motd /etc/motd
 
-    sudo systemctl enable --now sshd.socket
+    sudo systemctl enable --now sshd.service
 }
 
 #配置zsh
 function zsh_cfg() {
     #下载zsh相关包
-    yay -S zsh powerline-fonts zsh-syntax-highlighting zsh-autosuggestions autojump archlinuxcn/oh-my-zsh-git
+    yay -S powerline-fonts autojump oh-my-zsh-git
+    # yay -S zsh zsh-syntax-highlighting zsh-autosuggestions 
 
     # 安装.zshrc
     cp -v zsh/.zshrc ~/.zshrc
@@ -37,12 +38,13 @@ function tmux_cfg() {
         mkdir ~/.tmux
     fi
     cp -v tmux/.tmux.conf ~/.tmux.conf
+    sudo cp -v bin/terminal-tumx.sh /opt/bin
     yay -S tmux tmux-resurrect-git
 }
 
 #安装定制的SpaceVim for NeoVim
 function nvim_cfg() {
-    yay -S nvim xsel python-pynvim cmake ctags global cppcheck archlinuxcn/nerd-fonts-complete ripgrep
+    yay -S gvim neovim xsel python-pynvim cmake ctags global cppcheck archlinuxcn/nerd-fonts-complete ripgrep
     git clone git@gitee.com:mrbeardad/SpaceVim ~/.local/SpaceVim
     if [[ -d ~/.config/nvim ]] ;then
         mv ~/.config/{nvim,nvim.bak}
@@ -50,15 +52,12 @@ function nvim_cfg() {
     ln -s ~/.local/SpaceVim ~/.config/nvim
     if [[ ! -d ~/.SpaceVim.d ]] ;then
         mkdir ~/.SpaceVim.d
-    fi
-    if [[ -e ~/.SpaceVim.d/init.toml ]] ;then
+    elif [[ -e ~/.SpaceVim.d/init.toml ]] ;then
         mv ~/.SpaceVim.d/init.toml{,bak}
     fi
     cp -v ~/.local/SpaceVim/mod/init.toml ~/.SpaceVim.d
-    if [[ ! -d ~/.local/bin ]] ;then
-        mkdir ~/.local/bin
-    fi
-    cp -v bin/vim ~/.local/bin
+    cp -v ~/.config/nvim/vimrc.vim ~/.vimrc
+    sudo cp -v bin/vim-quickrun.sh /opt/bin
 }
 
 #安装额外的CLI工具、桌面软件、GNOME扩展
@@ -66,31 +65,28 @@ function extra_cfg() {
     #CHFS
     cd /tmp
     unzip /mnt/AUSU/packages/chfs-linux-amd64-1.8.zip
-    chmod 755 chfs-linux-amd64-1.8/chfs
-    sudo cp -v /opt/chfs-linux-amd64-1.8/chfs /opt/bin/chfs
+    chmod 755 chfs
+    sudo cp -v /opt/chfs /opt/bin/chfs
     cd -
     sudo cp chfs/chfs.{service,socket} /etc/systemd/system/
     sudo mkdir /srv/chfs
-    sudo chmod 755 /srv/chfs
+    sudo chmod 777 /srv/chfs
     sudo systemctl daemon-reload
     sudo systemctl enable --now chfs.socket
 
     #archibold
     sudo cp -v /mnt/AUSU/packages/archibold /opt
-    echo -e "\e[32m=====> GDB
-    Now, you can change your gdm background by run 'archibold login-backgroud /path/to/your/picture'"
 
     #CLI工具
-    yay -S htop iotop ncdu tldr cloc screenfetch ranger figlet cmatrix cheat dstat ntfs-3g cppman clang gdb
+    yay -S htop iotop ncdu tldr cloc screenfetch ranger figlet cmatrix cheat dstat cppman clang gdb
     cp -v gdb/.gdbinit ~
 
     #百度网盘，QQ，网易云音乐，搜狗拼音，WPS
-    yay -S baidunetdisk-bin deepin.com.qq.office vlc netease-cloud-music fcitx-sogoupinyin fcitx-im fcitx-configtool ssf2fcitx-git pinyin-completion wps-office ttf-wps-fonts flameshot google-chrome gnome-terminal-fedora alacritty
+    yay -S baidunetdisk-bin deepin.com.qq.office papper-flash vlc netease-cloud-music fcitx-sogoupinyin fcitx-im fcitx-configtool ssf2fcitx-git pinyin-completion wps-office ttf-wps-fonts flameshot google-chrome gnome-terminal-fedora alacritty
+    mkdir ~/.config/alacritty
     cp -v alacritty/alacritty.yml ~/.config/alacritty
     # git clone --depth=1 https://github.com/haotian-wang/google-access-helper ~/Downloads/google-access-helper
 
-    echo -e '\e[32=====> Chrome
-    Now, add google-access-helper to your google-chrome in devloper mode'
 
     #搜狗拼音配置
     echo -e 'export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIERS="@im=fcitx"' > ~/.xprofile
@@ -99,9 +95,9 @@ function extra_cfg() {
     #sudo sed -i '1a\export XMODIFIERS="@im=fcitx"\nexport QT_IM_MODULE="fcitx"\n' /usr/bin/wps
 
     #QQ配置，禁用ipv6，否则不显示图片
-    sudo echo "net.ipv6.conf.all.disable_ipv6 =1
+    sudo bash -c 'echo "net.ipv6.conf.all.disable_ipv6 =1
     net.ipv6.conf.default.disable_ipv6 =1
-    net.ipv6.conf.lo.disable_ipv6 =1" >> /etc/sysctl.conf
+    net.ipv6.conf.lo.disable_ipv6 =1" >> /etc/sysctl.conf'
     sudo sysctl -p /etc/sysctl.conf
     # 修改/opt/deepinwine/apps/Deepin-TIM/deepin.com.qq.office.desktop 禁用ipv6并使用optirun
 
@@ -110,7 +106,7 @@ function extra_cfg() {
 }
 
 
-# function main() { 安装完后就改个sudo配置，其他啥也不用动
+# function main() { 安装完后就改个sudo & fstab配置，其他啥也不用动
 
 # 系统配置
 timedatectl set-ntp 1 && timedatectl set-local-rtc 1
@@ -118,45 +114,49 @@ sudo systemctl disable --now bluetooth.service
 sudo systemctl disable --now org.cups.cupsd.service
 sudo systemctl enable --now fstrim.timer
 sudo sed -i '/\[Journal\]/a\SystemMaxUse=100M' /etc/systemd/journald.conf
-# sudo bash -c "mv /usr/local/* /opt && rmdir /usr/local && ln -s /opt /usr/local"
-echo -e "\e[32m=====> /etc/fstab\e[m
-Now you may want to edit your /etc/fstab to add a swap filesystem
-and change both <dump> and <pass> filed to 0 for all filesystem"
+sudo bash -c "mv /usr/local/* /opt && rmdir /usr/local && ln -s /opt /usr/local"
 
 # pacman配置
-sudo bash -c "echo 'Server = https://mirrors.cloud.tencent.com/manjaro/stable/\$repo/\$arch
-Server = https://mirrors.ustc.edu.cn/manjaro/stable/\$repo/\$arch
-Server = https://mirrors.tuna.tsinghua.edu.cn/manjaro/stable/\$repo/\$arch' > /etc/pacman.d/mirrorlist"
+sudo bash -c 'echo "Server = https://mirrors.cloud.tencent.com/manjaro/stable/$repo/$arch"
+ > /etc/pacman.d/mirrorlist'
 sudo cp /etc/pacman.d/mirrorlist{,.bak} # 有时候更新系统会把这个也更新了，备份一下
 sudo sed -i "/#Color/s/#//" /etc/pacman.conf
-sudo bash -c "echo '[archlinuxcn]
+sudo bash -c 'echo "[archlinuxcn]
 SigLevel = Optional TrustAll
-Server = https://mirrors.163.com/archlinux-cn/\$arch' >> /etc/pacman.conf"
+Server = https://mirrors.163.com/archlinux-cn/\$arch" >> /etc/pacman.conf'
 sudo pacman -Syu
-yay -S yay pacman-contrib-git aria2 expac base-devel
+pacman -S yay aria2 expac base-devel
 sudo systemctl enable --now paccache.timer
 
 # grub配置
 sudo cp grub/01_users /etc/grub.d
 sudo cp grub/user.cfg /boot/grub
 sudo sed -i '/--class os/s/--class os/--class os --unrestricted /' /etc/grub.d/{10_linux,30_os-prober}
-# sudo grub-mkconfig -o /boot/grub/grub.cfg
 yay -S grub-theme-manjaro
-echo -e "\e[32m=====> GRUB\e[m
-Now you may want to change your grub theme.Some version of Manjaro don't install grub-theme-manjaro.
-And you can may like to change the default .png picture. https://www.yasuotu.com/mgeshi provide online picture converter"
 
 # Nvidia配置
 # yay -S bumblebee virtualgl lib32-virtualgl lib32-primus primu linux54-bbswitch
 # sudo systemctl enable --now bumblebee
 # sudo gpasswd -A beardad bumblebee
-echo -e '\e[32m=====> patches
-Now, you may want to apply patches in patches directory'
 
 ssh_cfg
 zsh_cfg
 vim_cfg
 tmux_cfg
 extra_cfg
+
+echo -e "\e[32m=====> GRUB\e[m
+Now you may want to change your grub theme.Some version of Manjaro don't install grub-theme-manjaro.
+And you can may like to change the default .png picture. https://www.yasuotu.com/mgeshi provide online picture converter"
+echo -e '\e[32m=====> Patches\e[m
+Now, you may want to apply patches in patches directory'
+echo -e '\e[32m=====>  Shortcut Wrappers\e[m
+Now you need to bind the gnome shortcut for terminal-tmux & neovim-alacritty'
+echo -e "\e[32m=====> GDM\e[m
+Now, you can change your gdm background by run 'archibold login-backgroud /path/to/your/picture'"
+echo -e '\e[32=====> Chrome\e[m
+Now, add google-access-helper to your google-chrome in devloper mode'
+echo -e '\e[32=====> Neovim\e[m
+Now config your neovim by using files in DotFiles/vim'
 # }
 
--- init.sh
+++ init.sh
@@ -4,20 +4,19 @@
 function ssh_cfg() {
     # 添加git push <remote>需要的ssh配置
     cp -v ssh/ssh_config ~/.ssh/ssh_config
+
     # 设置sshd用于连接到该主机
     sudo cp -v ssh/sshd_config /etc/ssh/sshd_config
     sudo cp -v ssh/motd /etc/motd
 
-    sudo systemctl enable --now sshd.socket
-    echo -e "\e[32m=====> SSH\e[m
-    Now you need to push your ~/.ssh/id_ecdsa.pub to your github and gitee account."
+    sudo systemctl enable --now sshd.service
 }
 
 #配置zsh
 function zsh_cfg() {
     #下载zsh相关包
-    yay -S zsh powerline-fonts zsh-syntax-highlighting zsh-autosuggestions autojump
-    yay -S oh-my-zsh-git
+    yay -S powerline-fonts autojump oh-my-zsh-git
+    # yay -S zsh zsh-syntax-highlighting zsh-autosuggestions 
 
     # 安装.zshrc
     cp -v zsh/.zshrc ~/.zshrc
@@ -38,102 +37,69 @@ function tmux_cfg() {
         mkdir ~/.tmux
     fi
     cp -v tmux/.tmux.conf ~/.tmux.conf
+    sudo cp -v bin/terminal-tumx.sh /opt/bin
     yay -S tmux tmux-resurrect-git
 }
 
-
-#配置vim
-# function vim_cfg() {
-#     if [ ! -e ~/.vim ] ;then
-#         mkdir ~/.vim
-#     fi
-#
-#     if [ -e ~/.vim/vimrc ] ;then
-#         mv ~/.vim/vimrc{,.bak}
-#     elif [ -e ~/.vimrc ] ;then
-#         mv ~/.vimrc{,.bak}
-#     fi
-#
-#     if [ ! -e ~/.local/bin ] ;then
-#         mkdir ~/.local/bin
-#     fi
-#     cp  vim/vimrc ~/.vim/vimrc
-#     g++ -O3 -o ~/.local/bin vim/time.cpp
-#
-#     yay -S gvim vim-plug cmake ctags gperf vim-instant-markdown cppcheck archlinuxcn/nerd-fonts-complete
-#     #xsel ctags gtags gloobus-preview python-pynvim the_silver_searcher
-#     yay -S vim-youcompleteme-git
-#
-#     echo -e '\e[32m=====> VIM\e[m
-#     Now, launch your vim and run the command ":PlugInstall"
-# }
-
 #安装定制的SpaceVim for NeoVim
 function nvim_cfg() {
-    yay -S gvim nvim xsel python-pynvim cmake ctags global cppcheck archlinuxcn/nerd-fonts-complete ripgrep
-    git clone git@gitee.com:mrbeardad/SpaceVim ~/.local/SpaceVim
+    yay -S gvim neovim xsel python-pynvim cmake ctags global cppcheck archlinuxcn/nerd-fonts-complete ripgrep
+    # I have forked SpaceVim that do some modification. You can clone it on repo: mrbeardad/SpaceVim
+    git clone https://gitee.com/SpaceVim/SpaceVim ~/.local/SpaceVim
     if [[ -d ~/.config/nvim ]] ;then
         mv ~/.config/{nvim,nvim.bak}
-        ln -s ~/.local/SpaceVim ~/.config/nvim
     fi
+    ln -s ~/.local/SpaceVim ~/.config/nvim
 }
 
 #安装额外的CLI工具、桌面软件、GNOME扩展
 function extra_cfg() {
     #CHFS
-    echo -e '\e[32m=====> chfs\e[m
-    chfs is not installed by default. You may want to install it by yourself. Look up the init.sh source code.'
-    #curl -o ~/Downloads/chfs-linux-amd64-1.8.zip https://iscute.cn/tar/chfs/1.8/chfs-linux-amd64-1.8.zip
-    #cd /opt
-    #sudo unzip ~/Downloads/chfs-linux-amd64-1.8.zip
-    #chmod 755 chfs-linux-amd64-1.8/chfs
-    #sudo ln -s /opt/chfs-linux-amd64-1.8/chfs bin/chfs
-    #cd -
-    #sudo cp chfs/chfs.{service,socket} /etc/systemd/system/
-    #sudo mkdir /srv/chfs
-    #sudo chmod 755 /srv/chfs
-    #sudo systemctl daemon-reload
-    #sudo systemctl enable --now chfs.socket
+    # curl -o ~/Downloads/chfs-linux-amd64-1.8.zip https://iscute.cn/tar/chfs/1.8/chfs-linux-amd64-1.8.zip
+    # cd /tmp
+    # unzip ~/Downloads/chfs-linux-amd64-1.8.zip
+    # chmod 755 chfs
+    # sudo cp -v /opt/chfs /opt/bin/chfs
+    # cd -
+    # sudo cp chfs/chfs.{service,socket} /etc/systemd/system/
+    # sudo mkdir /srv/chfs
+    # sudo chmod 777 /srv/chfs
+    # sudo systemctl daemon-reload
+    # sudo systemctl enable --now chfs.socket
 
     #archibold
-    echo -e '\e[32m=====> archibold\e[m
-    archibold is not installed by default. You may want to install it by yourself. Look up the init.sh source code.'
     #sudo curl -o /opt/bin http://archibold.io/sh/archibold
     #sudo chmod 755 /opt/bin
     #echo -e "\e[32m=====> GDB
     #Now, you can change your gdm background by run 'archibold login-backgroud /path/to/your/picture'"
 
     #CLI工具
-    yay -S htop iotop ncdu tldr cloc screenfetch ranger figlet cmatrix cheat dstat ntfs-3g cppman clang gdb
+    yay -S htop iotop ncdu tldr cloc screenfetch ranger figlet cmatrix cheat dstat cppman clang gdb
     cp -v gdb/.gdbinit ~
 
     #百度网盘，QQ，网易云音乐，搜狗拼音，WPS
-    yay -S baidunetdisk-bin deepin.com.qq.office vlc netease-cloud-music fcitx-sogoupinyin fcitx-im fcitx-configtool ssf2fcitx-git pinyin-completion wps-office ttf-wps-fonts flameshot google-chrome gnome-terminal-fedora alacritty
+    yay -S baidunetdisk-bin deepin.com.qq.office pepper-falsh papper-flash vlc netease-cloud-music fcitx-sogoupinyin fcitx-im fcitx-configtool ssf2fcitx-git pinyin-completion wps-office ttf-wps-fonts flameshot google-chrome gnome-terminal-fedora alacritty
+    mkdir ~/.config/alacritty
     cp -v alacritty/alacritty.yml ~/.config/alacritty
     # git clone --depth=1 https://github.com/haotian-wang/google-access-helper ~/Downloads/google-access-helper
 
-    echo -e '\e[32=====> Chrome
-    Now, add google-access-helper to your google-chrome in devloper mode'
 
     #搜狗拼音配置
     echo -e 'export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIERS="@im=fcitx"' > ~/.xprofile
 
-    #WPS配置，现在好像不需要了。。
-    #sudo sed -i '1a\export XMODIFIERS="@im=fcitx"\nexport QT_IM_MODULE="fcitx"\n' /usr/bin/wps
-
     #QQ配置，禁用ipv6，否则不显示图片
-    sudo echo "net.ipv6.conf.all.disable_ipv6 =1
+    sudo bash -c 'echo "net.ipv6.conf.all.disable_ipv6 =1
     net.ipv6.conf.default.disable_ipv6 =1
-    net.ipv6.conf.lo.disable_ipv6 =1" >> /etc/sysctl.conf
+    net.ipv6.conf.lo.disable_ipv6 =1" >> /etc/sysctl.conf'
     sudo sysctl -p /etc/sysctl.conf
     # 修改/opt/deepinwine/apps/Deepin-TIM/deepin.com.qq.office.desktop 禁用ipv6并使用optirun
 
     #GNOME扩展
-    yay -S gtk-theme-macos-mojave adapta-gtk-theme-bin breeze-hacked-cursor-theme breeze-adapta-cursor-theme-git ttf-google-fonts-git tela-icon-theme-git gnome-shell-extension-coverflow-alt-tab gnome-shell-extension-system-monitor-git
+    yay -S gtk-theme-macos-mojave adapta-gtk-theme-bin breeze-hacked-cursor-theme breeze-adapta-cursor-theme-git ttf-google-fonts-git gnome-shell-extension-coverflow-alt-tab gnome-shell-extension-system-monitor-git
 }
 
 
-# function main() { 安装完后就改个sudo配置，其他啥也不用动
+# function main() { 安装完后就改个sudo & fstab配置，其他啥也不用动
 
 # 系统配置
 timedatectl set-ntp 1 && timedatectl set-local-rtc 1
@@ -141,44 +107,48 @@ sudo systemctl disable --now bluetooth.service
 sudo systemctl disable --now org.cups.cupsd.service
 sudo systemctl enable --now fstrim.timer
 sudo sed -i '/\[Journal\]/a\SystemMaxUse=100M' /etc/systemd/journald.conf
-# sudo bash -c "mv /usr/local/* /opt && rmdir /usr/local && ln -s /opt /usr/local"
-echo -e "\e[32m=====> /etc/fstab\e[m
-Now you may want to edit your /etc/fstab to add a swap filesystem
-and change both <dump> and <pass> filed to 0 for all filesystem"
+sudo bash -c "mv /usr/local/* /opt && rmdir /usr/local && ln -s /opt /usr/local"
 
 # pacman配置
-sudo bash -c "echo 'Server = https://mirrors.cloud.tencent.com/manjaro/stable/\$repo/\$arch
-Server = https://mirrors.ustc.edu.cn/manjaro/stable/\$repo/\$arch
-Server = https://mirrors.tuna.tsinghua.edu.cn/manjaro/stable/\$repo/\$arch' > /etc/pacman.d/mirrorlist"
+sudo bash -c 'echo "Server = https://mirrors.cloud.tencent.com/manjaro/stable/$repo/$arch"
+ > /etc/pacman.d/mirrorlist'
 sudo cp /etc/pacman.d/mirrorlist{,.bak} # 有时候更新系统会把这个也更新了，备份一下
 sudo sed -i "/#Color/s/#//" /etc/pacman.conf
-sudo bash -c "echo '[archlinuxcn]
+sudo bash -c 'echo "[archlinuxcn]
 SigLevel = Optional TrustAll
-Server = https://mirrors.163.com/archlinux-cn/\$arch' >> /etc/pacman.conf"
+Server = https://mirrors.163.com/archlinux-cn/\$arch" >> /etc/pacman.conf'
 sudo pacman -Syu
-yay -S yay pacman-contrib-git aria2 expac base-devel
+pacman -S yay aria2 expac base-devel
 sudo systemctl enable --now paccache.timer
 
 # grub配置
 sudo cp grub/01_users /etc/grub.d
-sudo cp grub/user.cfg /boot/grub
 sudo sed -i '/--class os/s/--class os/--class os --unrestricted /' /etc/grub.d/{10_linux,30_os-prober}
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-echo -e "\e[32m=====> GRUB\e[m
-Now you may want to change your grub theme.Some version of Manjaro don't install grub-theme-manjaro.
-And you can may like to change the default .png picture. https://www.yasuotu.com/mgeshi provide online picture converter"
+yay -S grub-theme-manjaro
 
 # Nvidia配置
 # yay -S bumblebee virtualgl lib32-virtualgl lib32-primus primu linux54-bbswitch
 # sudo systemctl enable --now bumblebee
 # sudo gpasswd -A beardad bumblebee
-echo -e '\e[32m=====> patches
-Now, you may want to apply patches in patches directory'
 
 ssh_cfg
 zsh_cfg
 vim_cfg
 tmux_cfg
 extra_cfg
+
+echo -e "\e[32m=====> GRUB\e[m
+Now you may want to change your grub theme.Some version of Manjaro don't install grub-theme-manjaro.
+And you can may like to change the default .png picture. https://www.yasuotu.com/mgeshi provide online picture converter"
+echo -e '\e[32m=====> Patches\e[m
+Now, you may want to apply patches in patches directory'
+echo -e '\e[32m=====>  Shortcut Wrappers\e[m
+Now you need to bind the gnome shortcut for terminal-tmux & neovim-alacritty'
+echo -e "\e[32m=====> GDM\e[m
+Now, you can change your gdm background by run 'archibold login-backgroud /path/to/your/picture'"
+echo -e '\e[32=====> Chrome\e[m
+Now, add google-access-helper to your google-chrome in devloper mode'
+echo -e '\e[32=====> Neovim\e[m
+Now config your neovim by using files in DotFiles/vim'
 # }
 
